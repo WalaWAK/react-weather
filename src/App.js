1ab1b186c8df7a0a2d1b11b0b95005e16d3a81d0
Myles <mylesmorales012@gmail.com>
2023-01-24 01:15:39 -0600

    containers

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
 
   return (
     <div className="app">
-
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Dallas</p>
+          </div>
+          <div className='temp'>
+            <h1>60°F</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+          </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>65°F</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            12 MPH
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
